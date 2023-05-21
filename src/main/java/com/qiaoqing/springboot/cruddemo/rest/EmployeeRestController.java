f4691191970c12a1ea093b734bb17aa5b45ba507
Qiaoqing Wu <wu000302@algonquinlive.com>
2023-05-21 18:31:07 -0400

    update EmployeeRestController

--- src/main/java/com/qiaoqing/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/qiaoqing/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,6 +2,7 @@ package com.qiaoqing.springboot.cruddemo.rest;
 
 import com.qiaoqing.springboot.cruddemo.dao.EmployeeDAO;
 import com.qiaoqing.springboot.cruddemo.entity.Employee;
+import com.qiaoqing.springboot.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +13,17 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     // quick and dirty: inject employee DAO
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
     }
 
     // expose "/employees" and return a list of employees
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 
 }
