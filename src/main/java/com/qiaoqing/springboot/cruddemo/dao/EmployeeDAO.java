d0fb09a8c9f425bf578f9bce35f71cde83bb3941
Qiaoqing Wu <wu000302@algonquinlive.com>
2023-05-21 18:37:51 -0400

    update interface EmployeeDAO with more CRUD functions

--- src/main/java/com/qiaoqing/springboot/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/qiaoqing/springboot/cruddemo/dao/EmployeeDAO.java
@@ -8,4 +8,10 @@ public interface EmployeeDAO {
 
     // return a list of employees
     List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }
